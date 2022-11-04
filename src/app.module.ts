f6ff30cd4b3f559713bf4e9cbff3a4f5128a8776
yh <546819964@qq.com>
2022-11-04 10:16:21 +0800

    first

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UserModule } from './server/user/user.module'
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test'), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
