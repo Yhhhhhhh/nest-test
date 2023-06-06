00a54e97b6be2da596c54da03e5980d4915a00b2
YH <yuhua@kuafu.tech>
2023-06-06 16:35:42 +0800

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,10 @@ import { AppService } from './app.service'
 import { UserModule } from './server/user/user.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test'), UserModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
